a5aa480d68bbc8f558c19aa1d2e565f115b8b52d
Ryota Kameoka <kameoka.ryota@gmail.com>
2014-10-19 05:57:38 +0900

    implement json output of user data

--- app/controllers/key_phrases_controller.rb
+++ app/controllers/key_phrases_controller.rb
@@ -5,17 +5,29 @@ class KeyPhrasesController < ApplicationController
   require 'json'
 
   def index
-    users_favs = Favorite.where(user_id: current_user.id).pluck(:content)
-    user_ids = User.where(room: current_user.room).where.not(id: current_user.id).pluck(:id)
-    json = []
+    user_id = params[:user_id].present? ? params[:user_id].to_i : current_user.id
+    c_user = User.find(user_id)
 
-    user_ids.each do |user_id|
-      other_favs = Favorite.where(user_id: user_id).pluck(:content)
-      user_info = User.find(user_id)
+    users = User.where(room: c_user.room).where.not(id: c_user.id)
+    json = []
+    levels = {}
+
+    favorites = Favorite.where(user_id: users.pluck(:id))
+    my_favorites = Favorite.where(user_id: user_id).pluck(:content)
+
+    raw_level_data = ActiveRecord::Base.connection.execute('SELECT user_id, count(*) as c FROM favorites WHERE content IN (SELECT content FROM favorites WHERE user_id = ' + user_id.to_s + ') GROUP BY user_id;')
+
+    raw_level_data.each do |record|
+      levels[record['user_id'].to_s] = record['c']
+    end
+
+    users.each do |user_info|
       user = {
+        id: user_info.id,
         icon: user_info.icon,
         name: user_info.nickname,
-        content: other_favs
+        level: levels[user_info.id.to_s] || 0,
+        content: favorites.where(user_id: user_info.id).pluck(:content) & my_favorites
       }
       json.push(user)
     end
