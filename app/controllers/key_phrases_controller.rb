b3e5bfa45ac81b08b83277269743370199b466e5
wataori <ropifuku@gmail.com>
2014-10-19 02:32:05 +0900

    fix including reply and rt when getting tweets

--- app/controllers/key_phrases_controller.rb
+++ app/controllers/key_phrases_controller.rb
@@ -1,5 +1,7 @@
 class KeyPhrasesController < ApplicationController
   require 'twitter'
+  # require "yahoo_keyphrase_api"
+  require 'net/http'
 
   def index
     @users_favs = Favorite.where(user_id: current_user.id)
@@ -11,8 +13,7 @@ class KeyPhrasesController < ApplicationController
   end
 
   def get_tweets
-
-    # @hoge = statuses_user_timeline
+    @hoge = []
 
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = 'Q0mqOUL0P5OB0dsZ6FdJVUWVB'
@@ -20,8 +21,23 @@ class KeyPhrasesController < ApplicationController
       config.access_token        = '519338540-yNAFQaEnCJ3D5Aq9dYg3aZI67FyHk6zucBCPaDB0'
       config.access_token_secret = 'XJCuo1eEHCNZf2ODNwO6SQnCctWd8vbCHNcI4TjaHbjzn'
     end
-    @hoge = client.user(current_user.nickname)
+
+    client.user_timeline(screen_name: current_user.nickname, count: 200, exclude_replies: true, include_rts: false).each do |tweet|
+      # Favorite.create(user_id: current_user.id, content: tweet.text, exclude_replies: true)
+      @hoge.push(tweet.text)
+    end
+
+    # YahooKeyphraseApi::Config.app_id = "dj0zaiZpPThhYk8xaVFJSkJtUiZzPWNvbnN1bWVyc2VjcmV0Jng9MTc-"
+    # ykp = YahooKeyphraseApi::KeyPhrase.new
 
     render json: @hoge
+  end
+
+  def word_search_result(target="")
+    @return_text = ""
+    @client.search(target, :result_type => "recent").take(200).each do |tweet|
+        @return_text += tweet.text.gsub(target, "")
+    end
+    return @return_text
   end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,3 +1,3 @@
 class Favorite < ActiveRecord::Base
-  belongs_to :user_id
+  belongs_to :user
 end
