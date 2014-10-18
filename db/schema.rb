a1459c0477b2968076eb48fc5f7d4c828b14340d
Ryota Kameoka <kameoka.ryota@gmail.com>
2014-10-19 04:14:43 +0900

    save user icon image path

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018145453) do
+ActiveRecord::Schema.define(version: 20141018190423) do
 
   create_table "favorites", force: true do |t|
     t.string   "content"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20141018145453) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "icon"
   end
 
 end
