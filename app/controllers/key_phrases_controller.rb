a5e856460aebd6bb829aec822dc8a6d07da9b38e
Ryota Kameoka <kameoka.ryota@gmail.com>
2014-10-19 03:40:55 +0900

    implement word save feature

--- app/controllers/key_phrases_controller.rb
+++ app/controllers/key_phrases_controller.rb
@@ -16,7 +16,6 @@ class KeyPhrasesController < ApplicationController
   def get_tweets
     @hoge = []
     res = {}
-    @aaaa = []
 
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = 'Q0mqOUL0P5OB0dsZ6FdJVUWVB'
@@ -26,52 +25,26 @@ class KeyPhrasesController < ApplicationController
     end
 
     client.user_timeline(screen_name: current_user.nickname, count: 200, exclude_replies: true, include_rts: false).each do |tweet|
-      # Favorite.create(user_id: current_user.id, content: tweet.text, exclude_replies: true)
 
       @hoge.push(tweet.text)
-      if @hoge.length === 30
+      if @hoge.length === 20
         tweets = @hoge.join(' ')
         url = ERB::Util.url_encode(tweets)
         res = OpenURI.open_uri('http://jlp.yahooapis.jp/KeyphraseService/V1/extract?appid=dj0zaiZpPThhYk8xaVFJSkJtUiZzPWNvbnN1bWVyc2VjcmV0Jng9MTc-&output=json&sentence=' + url)
       end
     end
-    @aaaa.push(JSON.load(res))
+    aaaa = JSON.load(res)
 
-    @a = []
-    @aaaa.each do |k, v|
-      @a.push(k)
+    words = aaaa.keys # 単語が入った配列を取得
+
+    words.each do |word| # 単語の情報を保存
+      Favorite.create(
+        content: word,
+        user_id: current_user.id
+      )
     end
 
-    # YahooKeyphraseApi::Config.app_id = "dj0zaiZpPThhYk8xaVFJSkJtUiZzPWNvbnN1bWVyc2VjcmV0Jng9MTc-"
-    # ykp = YahooKeyphraseApi::KeyPhrase.new
-
-    render json: @aaaa
+    render json: aaaa
   end
 
-  def http_request(method, uri, query_hash = {}, user = nil, pass = nil)
-    uri = URI.parse(uri) if uri.is_a? String
-    method = method.to_s.strip.downcase
-    query_string = (query_hash||{}).map{|k,v|
-      URI.encode(k.to_s) + "=" + URI.encode(v.to_s)
-    }.join("&")
-
-    if method == "post"
-      args = [Net::HTTP::Post.new(uri.path), query_string]
-    else
-      args = [Net::HTTP::Get.new(uri.path + (query_string.empty? ? "" : "?#{query_string}"))]
-    end
-    args[0].basic_auth(user, pass) if user
-
-    Net::HTTP.start(uri.host, uri.port) do |http|
-      return http.request(*args)
-    end
-  end
-
-  def word_search_result(target="")
-    @return_text = ""
-    @client.search(target, :result_type => "recent").take(200).each do |tweet|
-        @return_text += tweet.text.gsub(target, "")
-    end
-    return @return_text
-  end
 end
